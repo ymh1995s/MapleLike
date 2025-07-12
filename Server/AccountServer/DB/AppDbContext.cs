e602e374b6aca9daa804823ad0a6bc9ebc791430
DESKTOP-SLNADE2\user <qq11123@naver.com>
2025-07-13 06:41:42 +0900

    [feat] 인증서버(Web API) 생성과 가장 기본적인 DB 구축

---
+++ Server/AccountServer/DB/AppDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AccountServer.DB
+{
+    // 데이터베이스 컨텍스트 클래스, DbContext를 상속받아 데이터베이스와의 상호작용을 정의
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserDb> Users { get; set; }
+
+        // 로깅 설정을 위한 ILoggerFactory 인스턴스, 콘솔 출력
+        static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        // 의존성 주입을 위해 깡통 생성자 필요 : DbContext 옵션을 전달받기 위한 생성자
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // DbContext의 동작 설정
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+        }
+
+        // 데이터 모델 생성 시 추가적인 설정
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
---
+++ Server/AccountServer/DB/UserDb.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountServer.DB
+{
+    [Table("Users")]
+    public class UserDb
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required] // IS NOT NULL
+        public string PW { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? LastLogin { get; set; }
+    }
+}
